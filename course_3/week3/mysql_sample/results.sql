9a8d0785eb61568e74afb27c7c584c2c52ac1f7c
Шкиря Никита <nikitashkirya@gmail.com>
2020-01-30 11:53:33 +0600

    week3 example

--- course_3/week3/mysql_sample/results.sql
+++ course_3/week3/mysql_sample/results.sql
@@ -5,29 +5,29 @@ set names utf8;
 select * from product
 
 -- 2. Выбрать названия всех автоматизированных складов
-select ...
+select * from store
 
 -- 3. Посчитать общую сумму в деньгах всех продаж
-select ...
+select sum(total) from sale
 
 -- 4. Получить уникальные store_id всех складов, с которых была хоть одна продажа
-select ...
+select distinct(store_id) from sale
 
 
 -- 5. Получить уникальные store_id всех складов, с которых не было ни одной продажи
-select ...
+select store_id from store where store_id not in (select store_id from sale)
 
 -- 6. Получить для каждого товара название и среднюю стоимость единицы товара avg(total/quantity), если товар не продавался, он не попадает в отчет.
-select ...
+select p.name, avg(total / quantity)  from sale s join product p on p.product_id = s.product_id group by p.name
 
 -- 7. Получить названия всех продуктов, которые продавались только с единственного склада
-select ...
+select p.name from product p where (select count(distinct store_id) from sale s where product_id = p.product_id) = 1
 
 -- 8. Получить названия всех складов, с которых продавался только один продукт
-select ...
+select s.name from store s where (select count(distinct store_id) from sale where product_id = s.store_id) = 1
 
 -- 9. Выберите все ряды (все поля) из продаж, в которых сумма продажи (total) максимальна (равна максимальной из всех встречающихся)
-select ...
+select * from sale s where total = (select max(total) from sale)
 
 -- 10. Выведите дату самых максимальных продаж, если таких дат несколько, то самую раннюю из них
-select ...
+select date from sale s group by date order by count(*) desc, date limit 1
